c88461dca6c2cd791a8cd8e6b9eea7a472431d52
Lucas Camelo <53044534+lucasamsc@users.noreply.github.com>
2025-01-30 11:55:50 -0300

    Reestruturação de projeto

--- evaluation/bleu_eval.py
+++
@@ -1,4 +0,0 @@
-from nltk.translate.bleu_score import sentence_bleu
-
-def compute_bleu(generated, reference):
-    return sentence_bleu([reference.split()], generated.split())
--- models/model_loader.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from config import MODEL_NAMES
-
-def load_models():
-    tokenizers = {m: AutoTokenizer.from_pretrained(m) for m in MODEL_NAMES}
-    models = {m: AutoModelForCausalLM.from_pretrained(m) for m in MODEL_NAMES}
-    return tokenizers, models
